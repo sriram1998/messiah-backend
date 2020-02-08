5ea7f231644537a618c4a21408808754c76f6a58
salonipriyani <salonipriyani98@gmail.com>
2020-02-08 05:55:57 +0530

    adds autoincrement id

--- messiah_api/models.py
+++ messiah_api/models.py
@@ -3,13 +3,13 @@ from django.db import models
 
 # Create your models here.
 class Messes(models.Model):
-    messID = models.IntegerField(primary_key=True)
+    messID = models.AutoField(primary_key=True)
     messName = models.CharField(max_length=25)
     password = models.CharField(max_length=100)
 
 
 class Student(models.Model):
-    studentID = models.CharField(max_length=10, primary_key=True)
+    studentID = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
     rollNo = models.IntegerField()
     messID = models.ForeignKey(Messes, on_delete=models.CASCADE)
@@ -18,7 +18,7 @@ class Student(models.Model):
 
 
 class Menu(models.Model):
-    menuID = models.IntegerField(primary_key=True)
+    menuID = models.AutoField(primary_key=True)
     messID = models.ForeignKey(Messes, on_delete=models.CASCADE)
     date = models.DateField()
     day = models.CharField(max_length=10)
@@ -27,7 +27,7 @@ class Menu(models.Model):
 
 
 class foodStats(models.Model):
-    foodID = models.IntegerField(primary_key=True)
+    foodID = models.AutoField(primary_key=True)
     preparedQ = models.IntegerField()
     consumedQ = models.IntegerField()
     leftoverQ = models.IntegerField()
